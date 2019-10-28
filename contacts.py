78193b8cf65b706f019705b6460779980291be88
doran <doran.stephan@gmail.com>
2019-10-28 22:29:04 +0100

    add button in contacts

--- contacts.py
+++ contacts.py
@@ -15,14 +15,18 @@ def change_contact(right, is_selected, socketio, animate):
     env = Environment(loader=file_loader)
     template = env.get_template("contacts.html")
     class_select= []
+    display= []
 
     for i in range(0, len(is_selected)):
         if is_selected[i]:
             class_select.append("contact_selected")
+            display.append("")
         else:
             class_select.append("contact")
-    
-    output = template.render(is_selected_1=class_select[0], is_selected_2=class_select[1], is_selected_3=class_select[2])
+            display.append("none")
+
+    output = template.render(is_selected_1=class_select[0], is_selected_2=class_select[1], is_selected_3=class_select[2],
+                            display_1=display[0], display_2=display[1], display_3=display[2])
     print(is_selected)
         
     socketio.emit('html', {'number': output}, namespace='/test')
